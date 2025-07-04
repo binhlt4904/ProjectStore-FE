d6d9ac29d5d3096af1c8d60c26fb0b3684d92782
binhlt4904 <bicothu@gmail.com>
2025-07-04 16:54:04 +0700

    fix scroll

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -27,9 +27,9 @@ const ScrollToTopButton = ({ targetRef }) => {
   return (
     <button
       onClick={scrollToTop}
-      className="fixed bottom-6 right-6 z-50 bg-black text-white w-14 h-14 p-3 rounded-full shadow-lg hover:bg-gray-800 transition-all flex items-center justify-center"
+      className="fixed bottom-6 right-6 z-50 bg-black text-white w-12 h-12 p-3 rounded-full shadow-lg hover:bg-gray-800 transition-all flex items-center justify-center"
     >
-      <FaChevronUp className="w-6 h-6" />
+      <FaChevronUp className="w-5 h-5" />
     </button>
 
   );
